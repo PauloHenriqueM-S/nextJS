72712a16a108c13669c915994c06cc68be29b0c0
Paulo Henrique <phttm0501@gmail.com>
2023-10-20 10:17:06 -0400

    Att components

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,9 +1,19 @@
-import Link from "next/link";
+import Navegador from "@/components/Navegador";
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href="/estiloso">Estiloso</Link>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        height: "100vh",
+      }}
+    >
+      <Navegador texto="Estiloso" destino="/estiloso"></Navegador>
+      <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3"></Navegador>
+      <Navegador texto="JSX" destino="/jsx" cor="crimson"></Navegador>
     </div>
   );
 }
