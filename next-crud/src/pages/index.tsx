4cf8e9618bfeb217a50ea687c3e4fe91c97e0e37
Paulo Henrique <phttm0501@gmail.com>
2023-10-22 15:18:06 -0400

    update form and button

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Formulario from "@/components/Formulario";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
+import { useState } from "react";
 
 export default function Home() {
   const clientes = [
@@ -20,22 +21,41 @@ export default function Home() {
     console.log(`Excluir... ${cliente.nome}`);
   }
 
+  function salverCliente(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
   return (
     <div
       className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}
     >
       <Layout titulo="Cadastro Simples">
-        <div className="flex justify-end">
-          <Botao cor="green" className="mb-4">
-            Novo Cliente
-          </Botao>
-        </div>
-        <Tabela
-          clientes={clientes}
-          clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido}
-        />
-        <Formulario cliente={clientes[0]} />
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              <Botao
+                cor="green"
+                className="mb-4"
+                onClick={() => setVisivel("form")}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={clientes[0]}
+            clienteMudou={salverCliente}
+            cancelado={() => setVisivel("tabela")}
+          />
+        )}
       </Layout>
     </div>
   );
