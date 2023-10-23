f64a3020c3922bff07b9d78cb6a302162a211b6e
Paulo Henrique <phttm0501@gmail.com>
2023-10-23 11:21:21 -0400

    added firebase, db and client collection

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -6,6 +6,9 @@ import Cliente from "@/core/Cliente";
 import { useState } from "react";
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
   const clientes = [
     new Cliente("Ana", 34, "1"),
     new Cliente("Bia", 45, "2"),
@@ -14,18 +17,23 @@ export default function Home() {
   ];
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome);
+    setCliente(cliente);
+    setVisivel("form");
   }
 
   function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir... ${cliente.nome}`);
   }
 
-  function salverCliente(cliente: Cliente) {
-    console.log(cliente);
+  function novoCliente(cliente: Cliente) {
+    setCliente(Cliente.vazio());
+    setVisivel("form");
   }
 
-  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente);
+    setVisivel("tabela");
+  }
 
   return (
     <div
@@ -35,11 +43,7 @@ export default function Home() {
         {visivel === "tabela" ? (
           <>
             <div className="flex justify-end">
-              <Botao
-                cor="green"
-                className="mb-4"
-                onClick={() => setVisivel("form")}
-              >
+              <Botao cor="green" className="mb-4" onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
@@ -51,8 +55,8 @@ export default function Home() {
           </>
         ) : (
           <Formulario
-            cliente={clientes[0]}
-            clienteMudou={salverCliente}
+            cliente={cliente}
+            clienteMudou={salvarCliente}
             cancelado={() => setVisivel("tabela")}
           />
         )}
